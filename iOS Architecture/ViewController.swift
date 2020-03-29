37faf29f2ae8caeb8bbbf4bc8812ea17a825765c
Spencer Schoeben <spencer@netspencer.com>
2020-03-29 16:33:35 -0700

    init project

---
+++ iOS Architecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Architecture
+//
+//  Created by Spencer Schoeben on 3/29/20.
+//  Copyright © 2020 Spencer Schoeben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
